d0dc1778672f945156e49d8afbce1a30cfba56c9
Hiboys <101974795+Nobody-git@users.noreply.github.com>
2022-04-03 01:47:02 +0800

    修复BUG

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -16,5 +16,3 @@ echo "src-git dockerman https://github.com/lisaac/luci-app-dockerman.git" >> fee
 echo "src-git smpackage  https://github.com/kenzok8/small-package" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-
-cp -f feeds/smpackage/diy/zzz-default-settings package/default-settings/files/zzz-default-settings
