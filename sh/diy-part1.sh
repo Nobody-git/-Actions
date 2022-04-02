359a1bb56480b0d7d3e8581e8797e1731fdb1d66
Hiboys <101974795+Nobody-git@users.noreply.github.com>
2022-04-03 01:25:42 +0800

    修改BUG

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -13,5 +13,8 @@
 # Docker源
 echo "src-git dockerman https://github.com/lisaac/luci-app-dockerman.git" >> feeds.conf.default
 # passwall源
-echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
+echo "src-git smpackage  https://github.com/kenzok8/small-package" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+
+cp -f feeds/smpackage/diy/zzz-default-settings package/default-settings/files/zzz-default-settings
