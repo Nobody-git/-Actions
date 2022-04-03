fc28f78dc0d47a465fad4ed2f2abe879852a9339
Hiboys <101974795+Nobody-git@users.noreply.github.com>
2022-04-03 20:54:59 +0800

    修复BUG

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -15,4 +15,3 @@ echo "src-git dockerman https://github.com/lisaac/luci-app-dockerman.git" >> fee
 # passwall源
 echo "src-git smpackage  https://github.com/kenzok8/small-package" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
