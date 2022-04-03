b6a654f83dd8605a529472e990592ed1c10ae2d1
Hiboys <101974795+Nobody-git@users.noreply.github.com>
2022-04-03 21:39:48 +0800

    修复BUG

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -12,13 +12,8 @@ sed -i 's/192.168.1.1/172.10.10.250/g' package/base-files/files/bin/config_gener
 
 # 修改默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-cd ./feeds/luci/themes
-rm -rf  ./*
+rm -rf  ./feeds/luci/themes/*
 git clone https://github.com/Nobody-git/luci-theme-argon.git
-# sed -i 's/%D %V, %C/openwrt April by john/g' package/base-files/files/etc/banner
-
-# 修正连接数（by ベ七秒鱼ベ）
-# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
+sed -i 's/%D %V, %C/openwrt April by john/g'  package/base-files/files/etc/banner
 
 # 添加额外软件包
