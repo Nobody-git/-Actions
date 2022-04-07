36572668517146ecdb80bc7d01d9037313b48594
Hiboys <101974795+Nobody-git@users.noreply.github.com>
2022-04-08 03:25:00 +0800

    Update diy-part2.sh

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -8,12 +8,12 @@
 #
 
 # 自定义细节化配置
-cp -f $GITHUB_WORKSPACE/config/default-settings $GITHUB_WORKSPACE/openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/v$date1/April/g' $GITHUB_WORKSPACE/openwrt/package/lean/default-settings/files/zzz-default-settings
-cp -f $GITHUB_WORKSPACE/config/banner $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/banner
-sed -i 's/%D %V, %C/openwrt April by john/g' $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/banner
-cp -f $GITHUB_WORKSPACE/config/index_x86.htm $GITHUB_WORKSPACE/openwrt/package/lean/autocore/files/x86/index.htm
-patch -p1 < "$GITHUB_WORKSPACE/config/prereq-build_new.patch"
+cp -f config/default-settings openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/v$date1/April/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+cp -f config/banner openwrt/package/base-files/files/etc/banner
+sed -i 's/%D %V, %C/openwrt April by john/g' openwrt/package/base-files/files/etc/banner
+cp -f config/index_x86.htm openwrt/package/lean/autocore/files/x86/index.htm
+patch -p1 < "config/prereq-build_new.patch"
 
 # 修改默认IP
 sed -i 's/192.168.1.1/172.10.10.250/g' openwrt/package/base-files/files/bin/config_generate
