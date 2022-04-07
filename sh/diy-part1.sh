4808fca4d54b9b2b9eb99d701b6ea4e3794390d5
Nobody-git <john@winqi.cn>
2022-04-08 00:42:44 +0800

    修改错误

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -15,5 +15,5 @@
 # echo "src-git dockerman https://github.com/lisaac/luci-app-dockerman.git" >> feeds.conf.default
 
 # passwall源kenzok8大神代码库https://github.com/kenzok8
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packagese" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf.default
