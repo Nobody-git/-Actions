b01b3e45c38a146aa768a270902ea338756ea90f
Nobody-git <john@winqi.cn>
2022-04-19 00:07:55 +0800

    修改错误

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -22,9 +22,10 @@
 # 相关插件项目
 # echo "src-git jerryk https://github.com/jerrykuku/openwrt-package" >> feeds.conf.default
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-# # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default # passwall依赖于luci-app-passwall
-# # echo "src-git clash https://github.com/frainzy1477/luci-app-clash.git" >> feeds.conf.default
-# # echo "src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git" >> feeds.conf.default
+# echo "src-git clash https://github.com/frainzy1477/luci-app-clash.git" >> feeds.conf.default
+# echo "src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git" >> feeds.conf.default
 # echo "src-git istore https://github.com/linkease/istore.git" >> feeds.conf.default
+
 echo "src-git my https://github.com/Nobody-git/packages.git" >> feeds.conf.default
