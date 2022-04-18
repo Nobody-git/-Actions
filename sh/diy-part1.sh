226f2dfa226eef0a05545eef01888db5acf6fdef
Nobody-git <john@winqi.cn>
2022-04-18 23:57:50 +0800

    OK lean

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -17,19 +17,14 @@
 # passwall源kenzok8大神代码库https://github.com/kenzok8  
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 # echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf.default
-echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
+# echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
 
-
-# git clone https://github.com/fw876/helloworld.git package/ssr
-# git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
-# # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-# # git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
-# git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
-# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
-# # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# 相关插件项目
+# echo "src-git jerryk https://github.com/jerrykuku/openwrt-package" >> feeds.conf.default
+# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+# # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default # passwall依赖于luci-app-passwall
+# # echo "src-git clash https://github.com/frainzy1477/luci-app-clash.git" >> feeds.conf.default
+# # echo "src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git" >> feeds.conf.default
+# echo "src-git istore https://github.com/linkease/istore.git" >> feeds.conf.default
+echo "src-git my https://github.com/Nobody-git/packages.git" >> feeds.conf.default
