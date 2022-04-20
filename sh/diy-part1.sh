3791c3150bdea6802a7fe013b21f43e5b01a2378
Nobody-git <john@winqi.cn>
2022-04-20 21:50:02 +0800

    测试插件

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -17,7 +17,7 @@
 # passwall源kenzok8大神代码库https://github.com/kenzok8  
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 # echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf.default
-# echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
+echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
 
 # 相关插件项目
 # echo "src-git jerryk https://github.com/jerrykuku/openwrt-package" >> feeds.conf.default
@@ -27,4 +27,4 @@
 # echo "src-git clash https://github.com/frainzy1477/luci-app-clash.git" >> feeds.conf.default
 # echo "src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git" >> feeds.conf.default
 # echo "src-git istore https://github.com/linkease/istore.git" >> feeds.conf.default 
-echo "src-git my https://github.com/Nobody-git/packages.git" >> feeds.conf.default
+# echo "src-git my https://github.com/Nobody-git/packages.git" >> feeds.conf.default
